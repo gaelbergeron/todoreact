54b4677a2044524e76e768b6a1bd0217ba98bb47
gaelbergeron <gael.bergeron@gmail.com>
2015-10-05 15:52:18 -0700

    MVP generates a blank page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,4 @@
 class TodoController < ApplicationController
   def show
-    @todos = [
-      {id: 109, description: 'Finalize passion project', done: false}, 
-
-    ]
-    return @todos
   end
 end
